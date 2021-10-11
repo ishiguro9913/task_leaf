6f1e7762ec119c31a54e594f80c9b8a854322d80
ishiguro tasuku <ishitasukuro9913@gmail.com>
2021-10-11 23:47:05 +0900

    テストです　1011

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_26_030218) do
+ActiveRecord::Schema.define(version: 2021_10_10_163631) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
